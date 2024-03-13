aeff93d2adf74dac3be73565a2134b39b9931818
Hunter Smith <hunteralansmith@outlook.com>
2024-03-13 15:11:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 /* Information required for accessing my Spotify though last.fm */
-var api_key = "432bce050704e6d83883a144e5123809";
+var api_key = '432bce050704e6d83883a144e5123809';
+var user    = 'Peamupbubber'
 var cache   = new LastFMCache();
 var lastfm  = new LastFM({
     apiKey : api_key,
     cache  : cache
 });
 
-/* Get my recently listened song from lastfm */
+/* Get my recently/currently listened song */
 function getSpotifyInfo() {
-    lastfm.user.getRecentTracks({limit: 1, user: 'Peamupbubber', api_key: api_key}, {success: function(data){
+    lastfm.user.getRecentTracks({limit: 1, user: user, api_key: api_key}, {success: function(data){
         const track = data.recenttracks.track[0];
         var lt = "Was";
         if(track["@attr"] != undefined) {
