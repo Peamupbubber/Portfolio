467595fe4d4ca21633c7b07f08902129f45f0376
Hunter Smith <hunteralansmith@outlook.com>
2024-03-12 20:05:07 -0700

    added functionality to display info about my Spotify

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+var lastfm;
+
+function getSpotifyInfo() {
+    var api_key = "432bce050704e6d83883a144e5123809";
+
+    lastfm = new LastFM({ apiKey: api_key });
+
+    /* Get my recently listned song from lastfm */
+    lastfm.user.getRecentTracks({limit: 1, user: 'Peamupbubber', api_key: api_key}, {success: function(data){
+        const track = data.recenttracks.track[0];
+        document.getElementById("replace").innerText = track.name
+        }, error: function(code, message){
+        // Doesn't seem to report the error
+        document.getElementById("replace").innerText = "Couldn't get Spotify data :("
+    }});
+}
+
 function start() {
+    getSpotifyInfo();
+    
     var x = document.getElementsByClassName("projectSelectionGD");
     for (var i = 0; i < x.length; i++) {
-        x[i].style.display = "none"
+        x[i].style.display = "none";
     }
 }
 
@@ -9,9 +28,9 @@ function myFunction() {
     var x = document.getElementsByClassName("pages");
     for (var i = 0; i < x.length; i++) {
         if(x[i].style.display == "")
-            x[i].style.display = "none"
+            x[i].style.display = "none";
         else
-            x[i].style.display = ""
+            x[i].style.display = "";
     }
 }
 
@@ -19,27 +38,27 @@ function myFunction() {
 function displaySD() {
     var x = document.getElementsByClassName("projectSelectionGD");
     for (var i = 0; i < x.length; i++) {
-        x[i].style.display = "none"
+        x[i].style.display = "none";
     }
 
     x = document.getElementsByClassName("projectSelectionSD");
     for (var i = 0; i < x.length; i++) {
-        x[i].style.display = ""
+        x[i].style.display = "";
     }
 }
 
 function displayGD() {
     var x = document.getElementsByClassName("projectSelectionSD");
     for (var i = 0; i < x.length; i++) {
-        x[i].style.display = "none"
+        x[i].style.display = "none";
     }
 
     x = document.getElementsByClassName("projectSelectionGD");
     for (var i = 0; i < x.length; i++) {
-        x[i].style.display = ""
+        x[i].style.display = "";
     }
 }
 
 function displayAboutMe() {
-    alert("About Me clicked!")
+    alert("About Me clicked!");
 }
